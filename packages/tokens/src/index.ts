324098e422a905019ad5f4d16a08827555bc0d74
LasaleFamine <info@godev.space>
2022-01-28 21:50:33 +0100

    feat: added tokens

---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,41 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+import CssBezier from './transformers/css-bezier';
+import HexHslValues from './transformers/hex-hslvalues';
+import SizePxToRem from './transformers/px-rem';
+import SizePxToRootEm from './transformers/px-rootem';
+
+const StyleDictionary = require('style-dictionary').extend('tokens.config.json');
+
+/**
+ * Register custom transformers to process token values for
+ * the web platform
+ */
+StyleDictionary.registerTransform(HexHslValues);
+StyleDictionary.registerTransform(SizePxToRem);
+StyleDictionary.registerTransform(SizePxToRootEm);
+StyleDictionary.registerTransform(CssBezier);
+
+/**
+ * Add the custom transformers to a new transformGroup `custom-web`
+ * used inside tokens.config.json
+ */
+StyleDictionary.registerTransformGroup({
+  name: 'custom-web',
+  transforms: [
+    'attribute/cti',
+    'name/cti/kebab',
+    'time/seconds',
+    'content/icon',
+    'size/px-rootem',
+    'size/px-rem',
+    'color/hslvalue',
+    'easing/cubic-bezier',
+  ],
+});
+
+/**
+ * Manually run StyleDictionary for all the configured platforms
+ */
+StyleDictionary.buildAllPlatforms();
