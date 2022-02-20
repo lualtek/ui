87cdd6414c027b42f4c06e124cb7b46401705ca0
Mattia Astorino <equinusocio@users.noreply.github.com>
2022-02-20 11:33:11 +0100

    feat: loading components (#21)
    * feat: add Skeleton
    
    * feat: add LinearProgress
    
    * feat: add CircularProgress

---
+++ packages/react-components/src/components/linear-progress/linear-progress.stories.tsx
@@ -0,0 +1,32 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { LinearProgress } from './linear-progress';
+
+const story: ComponentMeta<typeof LinearProgress> = {
+  title: 'Components/Loading/Linear progress',
+  component: LinearProgress,
+  args: {
+    max: 100,
+    showProgress: true,
+    dimension: 'regular',
+  },
+  argTypes: {
+    value: {
+      control: { type: 'range' },
+    },
+    dimension: {
+      options: ['regular', 'big'],
+      control: { type: 'radio' },
+    },
+  },
+};
+
+export default story;
+
+const Template: ComponentStory<typeof LinearProgress> = args => <LinearProgress {...args} />;
+
+export const Determinate = Template.bind({});
+Determinate.args = {
+  value: 40,
+};
+export const Indeterminate = Template.bind({});
