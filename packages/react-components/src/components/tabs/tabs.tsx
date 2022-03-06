9f8fa7f090e72c453dd3551ebd4151fd66c16199
Mattia Astorino <astorino.mattia@gmail.com>
2022-03-06 16:50:10 +0100

    feat: add tab style

--- packages/react-components/src/components/tabs/tabs.tsx
+++ packages/react-components/src/components/tabs/tabs.tsx
@@ -1,6 +1,12 @@
 import * as TabsPrimitive from '@radix-ui/react-tabs';
-import { Children, isValidElement } from 'react';
+import { domMax, LazyMotion, m } from 'framer-motion';
+import {
+  Children, isValidElement, useCallback, useState,
+} from 'react';
 
+import { Button } from '@/components';
+
+import styles from './tabs.module.css';
 import { TabPanel } from './tabs-panel';
 
 export type TabsProps = TabsPrimitive.TabsProps & Record<string, unknown>;
@@ -8,23 +14,46 @@ export type TabsProps = TabsPrimitive.TabsProps & Record<string, unknown>;
 export const Tabs = ({
   className,
   children,
+  onValueChange,
+  defaultValue,
   ...otherProps
 }: TabsProps) => {
-  const renderedChildren = Children.toArray(children).filter(Boolean);
+  const [activeItem, setActiveItem] = useState<string>(defaultValue ?? '');
+
+  const handleOnVlaueChange = useCallback(
+    (value: string) => {
+      onValueChange?.(value);
+      setActiveItem(value);
+    },
+    [onValueChange],
+  );
 
   return (
-    <TabsPrimitive.Root {...otherProps}>
-      <TabsPrimitive.List>
-        {Children.map(renderedChildren, child => isValidElement(child) && (
-          <TabsPrimitive.Trigger
-            value={child.props.value}
-            disabled={child.props.disabled}
-          >
-            {child.props.label}
-          </TabsPrimitive.Trigger>
-        ))}
-      </TabsPrimitive.List>
-      {renderedChildren}
+    <TabsPrimitive.Root
+      defaultValue={defaultValue}
+      onValueChange={handleOnVlaueChange}
+      {...otherProps}
+    >
+      <LazyMotion features={domMax} strict>
+        <TabsPrimitive.List className={styles.List}>
+          {Children.map(children, child => isValidElement(child) && (
+            <TabsPrimitive.Trigger
+              value={child.props.value}
+              disabled={child.props.disabled}
+              className={styles.Trigger}
+              asChild
+            >
+              <Button kind="flat">
+                {child.props.label}
+                {(child.props.value === activeItem) && 'active' && (
+                  <m.span className={styles.Highlight} layoutId="highlight" />
+                )}
+              </Button>
+            </TabsPrimitive.Trigger>
+          ))}
+        </TabsPrimitive.List>
+        {children}
+      </LazyMotion>
     </TabsPrimitive.Root>
   );
 };
