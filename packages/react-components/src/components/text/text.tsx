09697ccf77bb251a9d96ede6021477568a1aed51
Mattia Astorino <astorino.mattia@gmail.com>
2022-10-11 16:46:26 +0200

    fix: responsive 12 text size

--- packages/react-components/src/components/text/text.tsx
+++ packages/react-components/src/components/text/text.tsx
@@ -77,7 +77,7 @@ export const Text = forwardRef(({
       data-text-sentiment={sentiment}
       data-text-dimmed={dimmed}
       data-text-line-height={lineHeight}
-      data-text-responsive={size === 14 ? false : responsive}
+      data-text-responsive={size === 12 ? false : responsive}
       className={clsx(styles.Text, className)}
       style={{ ...dynamicStyle, ...style }}
       {...otherProps}
