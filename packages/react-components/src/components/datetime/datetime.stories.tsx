a2211c500353c8af7f6f5cfcd621c380f75d2972
Mattia Astorino <astorino.mattia@gmail.com>
2023-03-21 13:41:53 +0100

    feat: add Datetime

---
+++ packages/react-components/src/components/datetime/datetime.stories.tsx
@@ -0,0 +1,27 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { Datetime } from './datetime';
+
+const story: ComponentMeta<typeof Datetime> = {
+  title: 'Widgets/Datetime',
+  component: Datetime,
+  argTypes: {
+    locale: { type: 'string' },
+  },
+  args: {
+    locale: 'it-IT',
+    date: '2021-12-15T16:00:32.507981+00:00',
+  },
+};
+
+export default story;
+
+const Template: ComponentStory<typeof Datetime> = args => <Datetime {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  date: '2021-12-15T16:00:32.507981+00:00',
+  options: {
+    year: '2-digit',
+  },
+};
---
+++ packages/react-components/src/components/datetime/datetime.tsx
@@ -0,0 +1,47 @@
+import {
+  FC, HTMLAttributes, useEffect, useState,
+} from 'react';
+
+export type DatetimeProps = HTMLAttributes<HTMLTimeElement> & {
+  /**
+   * The date to format and display.
+   */
+  date: string;
+  /**
+   * Set the locale to use to format the date.
+   */
+  locale?: string | string[];
+  /**
+   * Customize the date format by passing options from Intl.DateTimeFormat
+   *
+   * Read more: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat
+   */
+  options?: Intl.DateTimeFormatOptions;
+}
+
+export const Datetime: FC<DatetimeProps> = ({
+  date,
+  locale = 'en-US',
+  options,
+  ...otherProps
+}) => {
+  const [datetime, setDateTime] = useState<string>('');
+
+  useEffect(() => {
+    const timeDate: Date = new Date(date);
+    const humanDate = new Intl.DateTimeFormat(locale, {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      ...options,
+    }).format(timeDate);
+
+    setDateTime(humanDate);
+  }, [date, locale, options]);
+
+  return (
+    <time dateTime={date} {...otherProps}>
+      {datetime}
+    </time>
+  );
+};
