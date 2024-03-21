73b15d4a83c2f5b277a22b141fa23e3f74d64bb2
Mattia Astorino <astorino.mattia@gmail.com>
2024-03-21 10:26:43 +0100

    feat: add skeleton loading to Avatar when src is set

--- packages/react-components/src/components/avatar/avatar.tsx
+++ packages/react-components/src/components/avatar/avatar.tsx
@@ -1,5 +1,8 @@
+import { TokensTypes } from '@lualtek/tokens/platforms/web';
 import clsx from 'clsx';
-import { forwardRef, ImgHTMLAttributes } from 'react';
+import { forwardRef, ImgHTMLAttributes, useState } from 'react';
+
+import { Skeleton } from '@/components';
 
 import styles from './avatar.module.css';
 
@@ -15,47 +18,69 @@ export type AvatarProps = ImgHTMLAttributes<HTMLImageElement> & {
   dimension?: 'small' | 'regular' | 'big';
 }
 
+type SkeletonSizeType = Record<
+NonNullable<AvatarProps['dimension']>,
+Exclude<TokensTypes['space'], string>
+>;
+
+const SkeletonSize: SkeletonSizeType = {
+  small: 24,
+  regular: 40,
+  big: 48,
+};
+
 export const Avatar = forwardRef<HTMLImageElement, AvatarProps>(({
   className,
   src,
   dimension = 'regular',
   ...otherProps
-}, forwardedRef) => (
-  <picture
-    className={clsx(styles.Avatar, className)}
-    data-avatar-dimension={dimension}
-  >
-    <svg
-      aria-hidden="true"
-      className={styles.Placeholder}
-      height={18}
-      viewBox="0 0 12 18"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
+}, forwardedRef) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <picture
+      className={clsx(styles.Avatar, className)}
+      data-loading={isLoading}
+      data-avatar-dimension={dimension}
     >
-      <path
-        opacity={0.7}
-        d="M6 0C3.795 0 2 1.794 2 4s1.795 4 4 4 4-1.794 4-4-1.795-4-4-4z"
-        fill="var(--highlight-brand-foreground)"
-      />
-      <path
-        d="M8.4 9H3.6C1.612 9 0 10.575 0
+      {!src && (
+        <svg
+          aria-hidden="true"
+          className={styles.Placeholder}
+          height={18}
+          viewBox="0 0 12 18"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            opacity={0.7}
+            d="M6 0C3.795 0 2 1.794 2 4s1.795 4 4 4 4-1.794 4-4-1.795-4-4-4z"
+            fill="var(--highlight-brand-foreground)"
+          />
+          <path
+            d="M8.4 9H3.6C1.612 9 0 10.575 0
         12.531v5.126C1.814 18.517 3.85 19 6
         19s4.186-.483 6-1.343v-5.126C12 10.581 10.394 9 8.4 9z"
-        fill="var(--highlight-brand-foreground)"
-      />
-    </svg>
+            fill="var(--highlight-brand-foreground)"
+          />
+        </svg>
+      )}
 
-    {src && (
-      <img
-        ref={forwardedRef}
-        draggable={false}
-        alt=""
-        src={src}
-        {...otherProps}
-      />
-    )}
-  </picture>
-));
+      {src && isLoading && <Skeleton width={SkeletonSize[dimension]} height={SkeletonSize[dimension]} />}
+
+      {src && (
+        <img
+          ref={forwardedRef}
+          draggable={false}
+          hidden={isLoading}
+          onLoad={() => setIsLoading(false)}
+          alt=""
+          src={src}
+          {...otherProps}
+        />
+      )}
+    </picture>
+  );
+});
 
 Avatar.displayName = 'Avatar';
