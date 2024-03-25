93eb090f7ef7394dd6c41d83ec09d564f27e3d76
Mattia Astorino <astorino.mattia@gmail.com>
2024-03-25 10:12:42 +0100

    chore: update stories

--- packages/react-components/src/components/blank-button/blank-button.stories.tsx
+++ packages/react-components/src/components/blank-button/blank-button.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
+import { Stack } from '../..';
 import { BlankButton } from './blank-button';
 
 const meta = {
@@ -7,7 +8,7 @@ const meta = {
   component: BlankButton,
   render: args => (
     <BlankButton onClick={() => alert('Clicked')} {...args}>
-      <img data-chromatic="ignore" src="https://api.lorem.space/image/game?w=150&h=220" alt="" />
+      <img data-chromatic="ignore" height={200} src="https://mighty.tools/mockmind-api/content/human/40.jpg" alt="" />
     </BlankButton>
   ),
 } satisfies Meta<typeof BlankButton>;
@@ -17,3 +18,10 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default = {} satisfies Story;
+export const AsStack = {
+  render: args => (
+    <Stack vPadding={40} hPadding={40} as={BlankButton} onClick={() => alert('Clicked')} {...args}>
+      <img data-chromatic="ignore" height={200} src="https://mighty.tools/mockmind-api/content/human/40.jpg" alt="" />
+    </Stack>
+  ),
+} satisfies Story;
