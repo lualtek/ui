239fa4c36638509f2bf249586e541c47322876a9
Mattia Astorino <astorino.mattia@gmail.com>
2024-04-30 16:20:43 +0200

    chore: test di cose

--- packages/charts/src/components/test/test.stories.tsx
+++ packages/charts/src/components/test/test.stories.tsx
@@ -1,14 +1,53 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import {
+  AnimatedLineSeries,
+} from '@visx/xychart';
 
-import { Test } from './test';
+import { LinearChart } from './test';
+
+type Data = { x: string; y: number };
+type Dataset = Data[];
+
+const data1: Dataset = [
+  { x: '2020-01-01', y: 50 },
+  { x: '2020-01-02', y: 10 },
+  { x: '2020-01-03', y: 20 },
+];
+
+const data2: Dataset = [
+  { x: '2020-01-01', y: 30 },
+  { x: '2020-01-02', y: 40 },
+  { x: '2020-01-03', y: 80 },
+];
+
+const accessors = {
+  xAccessor: (d: Data) => d.x,
+  yAccessor: (d: Data) => d.y,
+};
 
 const meta = {
   title: 'Charts/Linear',
-  component: Test,
+  component: LinearChart,
   args: {
-
+    accessors,
   },
-} satisfies Meta<typeof Test>;
+  render: args => (
+    <LinearChart {...args}>
+      <AnimatedLineSeries
+        dataKey="data1"
+        data={data1}
+        xAccessor={accessors.xAccessor}
+        yAccessor={accessors.yAccessor}
+      />
+      <AnimatedLineSeries
+        dataKey="data2"
+        data={data2}
+        xAccessor={accessors.xAccessor}
+        yAccessor={accessors.yAccessor}
+      />
+    </LinearChart>
+  ),
+} satisfies Meta<typeof LinearChart>;
 
 export default meta;
 
--- packages/charts/src/components/test/test.tsx
+++ packages/charts/src/components/test/test.tsx
@@ -1,17 +1,50 @@
 import { FCChildrenClass } from '@lualtek/react-components';
-import clsx from 'clsx';
+import {
+  AnimatedAxis,
+  AnimatedGrid,
+  Tooltip,
+  XYChart,
+} from '@visx/xychart';
 
-import styles from './test.module.css';
+export type LinearChartProps = {
+  accessors: {
+    xAccessor: (d: any) => any;
+    yAccessor: (d: any) => any;
+  };
+}
 
-export type TestProps = Record<string, unknown>
-
-export const Test: FCChildrenClass = ({
+export const LinearChart: FCChildrenClass<LinearChartProps> = ({
   className,
+  children,
+  accessors,
   ...otherProps
 }) => (
-  <div className={clsx(styles.Test, className)} {...otherProps}>
-    CONTENT
-  </div>
+  <XYChart
+    height={300}
+    xScale={{ type: 'point' }}
+    yScale={{ type: 'linear' }}
+    {...otherProps}
+  >
+    <AnimatedAxis orientation="bottom" />
+    <AnimatedGrid columns={false} numTicks={4} />
+    {children}
+    <Tooltip
+      snapTooltipToDatumX
+      snapTooltipToDatumY
+      showVerticalCrosshair
+      showSeriesGlyphs
+      renderTooltip={({ tooltipData, colorScale }) => tooltipData?.nearestDatum && (
+        <div>
+          <div style={{ color: colorScale?.(tooltipData.nearestDatum.key) }}>
+            {tooltipData.nearestDatum.key}
+          </div>
+          {accessors.xAccessor(tooltipData.nearestDatum.datum)}
+          {', '}
+          {accessors.yAccessor(tooltipData.nearestDatum.datum)}
+        </div>
+      )}
+    />
+  </XYChart>
 );
 
-Test.displayName = 'Test';
+LinearChart.displayName = 'LinearChart';
