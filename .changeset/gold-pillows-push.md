9d90ed6ecd6ede844fe2c7527d4fd358f3dd2dc5
Mattia Astorino <astorino.mattia@gmail.com>
2024-06-12 08:37:31 +0200

    fix: add missing export for ReferenceArea and ReferenceLine components

---
+++ .changeset/gold-pillows-push.md
@@ -0,0 +1,5 @@
+---
+"@lualtek/charts": patch
+---
+
+Add missing export for ReferenceArea and ReferenceLine components
--- packages/charts/src/components/index.ts
+++ packages/charts/src/components/index.ts
@@ -1,2 +1,5 @@
 export * from './bar-chart';
+export type { ReferenceAreaProps } from './base-chart';
+export type { ReferenceLineProps } from './base-chart';
+export { ReferenceArea, ReferenceLine } from './base-chart';
 export * from './line-chart';
