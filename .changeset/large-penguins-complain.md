ecefc2d9c047bf1731e18ef3b4b72dcc49b61b13
Mattia Astorino <astorino.mattia@gmail.com>
2024-07-03 08:16:25 +0200

    feat: add missing EmojiClickData

---
+++ .changeset/large-penguins-complain.md
@@ -0,0 +1,5 @@
+---
+"@lualtek/react-components": patch
+---
+
+Add missing EmojiClickData type
--- packages/react-components/src/components/emoji-picker/index.ts
+++ packages/react-components/src/components/emoji-picker/index.ts
@@ -1,2 +1,3 @@
 export type { EmojiPickerProps } from './emoji-picker';
 export { EmojiPicker } from './emoji-picker';
+export type { EmojiClickData } from 'emoji-picker-react';
