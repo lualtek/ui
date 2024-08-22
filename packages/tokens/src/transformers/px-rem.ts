c7be99900f7571ba6a8ecdd2152afe91513aec68
Mattia Astorino <astorino.mattia@gmail.com>
2024-08-22 08:33:41 +0200

    chore: rename px-rem transformer

--- packages/tokens/src/transformers/px-rem.ts
+++ packages/tokens/src/transformers/px-rem.ts
@@ -3,7 +3,7 @@ import type { Transform } from 'style-dictionary/types';
 const pxToRem: Transform = {
   name: 'size/px-rem',
   type: 'value',
-  filter: prop => prop.$type === 'size/rem',
+  filter: prop => prop.$type === 'dimension-px-to-rem',
   transform: (token, options) => {
     const baseRootSize = options?.basePxFontSize as number ?? 16;
     return `${(token.$value / baseRootSize).toFixed(2)}rem`;
