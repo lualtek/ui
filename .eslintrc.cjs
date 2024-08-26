18b1424734e3b7b3fa5d8c5118e44d9e28aec632
Mattia Astorino <astorino.mattia@gmail.com>
2024-08-26 12:50:37 +0200

    chore: update eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['@lasalefamine/eslint-config', '@lasalefamine/eslint-config/hooks'],
   parserOptions: {
     project: './tsconfig.eslint.json',
+    tsconfigRootDir: __dirname,
   },
   // Disable extraneous deps for monorepo incompatibility
   rules: {
