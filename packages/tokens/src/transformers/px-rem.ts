d44ae57c5cf6295e33b85aec23215b07ec5cb84b
Mattia Astorino <astorino.mattia@gmail.com>
2024-09-16 10:40:57 +0200

    chore: style

--- packages/tokens/src/transformers/px-rem.ts
+++ packages/tokens/src/transformers/px-rem.ts
@@ -2,10 +2,10 @@ import StyleDictionary from 'style-dictionary';
 import type { ValueTransform } from 'style-dictionary/types';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-const pxToRemBuiltin = StyleDictionary.hooks.transforms['size/pxToRem'];
+const pxToRemBuiltin = StyleDictionary.hooks.transforms['size/pxToRem'] as ValueTransform;
 
 const pxToRem: ValueTransform = {
-  ...pxToRemBuiltin as ValueTransform,
+  ...pxToRemBuiltin,
   filter: prop => prop.$type === 'dimension-px-to-rem',
   name: 'size/pxToRem',
 };
