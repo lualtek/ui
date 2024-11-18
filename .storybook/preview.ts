9f6c837f2cb9598310dc89a861103f2d0cae6d65
Mattia Astorino <astorino.mattia@gmail.com>
2024-11-18 13:41:01 +0100

    chore: remove @storybook/addon-themes

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,24 +2,12 @@ import '@lualtek/themes/web';
 import '../packages/react-components/src/core.css';
 import './overrides.css';
 
-import { withThemeByDataAttribute } from '@storybook/addon-themes';
-import type { ReactRenderer } from '@storybook/react';
 import { Preview } from '@storybook/react';
 import { themes } from '@storybook/theming';
 
 import lualtekTheme from './lualtek-theme';
 
 const preview: Preview = {
-  decorators: [
-    withThemeByDataAttribute<ReactRenderer>({
-      themes: {
-        auto: 'auto',
-        light: 'light',
-        dark: 'dark',
-      },
-      defaultTheme: 'dark',
-    }),
-  ],
   parameters: {
     backgrounds: { disable: true },
     docs: {
