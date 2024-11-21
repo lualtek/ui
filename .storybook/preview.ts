19eadb8ddcd20539368788f5fd4df261d682ec4f
Mattia Astorino <astorino.mattia@gmail.com>
2024-11-21 12:17:31 +0100

    chore: update deps

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,12 +2,24 @@ import '@lualtek/themes/web';
 import '../packages/react-components/src/core.css';
 import './overrides.css';
 
+import { withThemeByDataAttribute } from '@storybook/addon-themes';
+import type { ReactRenderer } from '@storybook/react';
 import { Preview } from '@storybook/react';
 import { themes } from '@storybook/theming';
 
 import lualtekTheme from './lualtek-theme';
 
 const preview: Preview = {
+  decorators: [
+    withThemeByDataAttribute<ReactRenderer>({
+      themes: {
+        auto: 'auto',
+        light: 'light',
+        dark: 'dark',
+      },
+      defaultTheme: 'dark',
+    }),
+  ],
   parameters: {
     backgrounds: { disable: true },
     docs: {
