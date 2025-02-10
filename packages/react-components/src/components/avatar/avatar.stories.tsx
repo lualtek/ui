40807643754434017a182cb41ba401bcdb531254
Mattia Astorino <astorino.mattia@gmail.com>
2025-02-10 17:12:58 +0100

    chore: update avatar story

--- packages/react-components/src/components/avatar/avatar.stories.tsx
+++ packages/react-components/src/components/avatar/avatar.stories.tsx
@@ -1,21 +1,13 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Stack } from '../..';
 import { Avatar } from './avatar';
 
 const meta = {
   title: 'Widgets/Avatar',
   component: Avatar,
   args: {
+    dimension: 'big',
     src: 'https://mighty.tools/mockmind-api/content/human/72.jpg',
-  },
-  render: function Renderer(args) {
-    return (
-      <Stack direction="row" fill={false} inline columnGap={8}>
-        <Avatar {...args} />
-        <Avatar {...args} src="" />
-      </Stack>
-    );
   },
 } satisfies Meta<typeof Avatar>;
 
@@ -23,20 +15,12 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Small = {
-  args: {
-    dimension: 'small',
-  },
+export const Default = {
+  args: {},
 } satisfies Story;
 
-export const Regular = {
+export const NoImage = {
   args: {
-    dimension: 'regular',
-  },
-} satisfies Story;
-
-export const Big = {
-  args: {
-    dimension: 'big',
+    src: '',
   },
 } satisfies Story;
