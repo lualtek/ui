9ebb4baba5b98d3b87780c1145e338ba42a44346
Mattia Astorino <astorino.mattia@gmail.com>
2025-08-22 15:03:24 +0200

    chore: update trigger

--- packages/react-components/src/components/swipe-actions/swipe-trigger.tsx
+++ packages/react-components/src/components/swipe-actions/swipe-trigger.tsx
@@ -1,6 +1,6 @@
 import type { PanInfo } from 'motion/react';
 import { animate, motion } from 'motion/react';
-import React, { FC, PropsWithChildren } from 'react';
+import React, { FC, PropsWithChildren, useRef } from 'react';
 
 import styles from './swipe-actions.module.css';
 import { useSwipeActions } from './swipe-actions-context';
@@ -11,9 +11,24 @@ export type SwipeTriggerProps = {
 
 export const SwipeTrigger: FC<PropsWithChildren<SwipeTriggerProps>> = ({ children }) => {
   const { x, actionsWidth } = useSwipeActions();
+  /**
+   * Ref to determine if the action is a drag
+   */
+  const wasDraggedRef = useRef(false);
+
+  const handleDragStart = () => {
+    /**
+     * The onDragStart event is triggered only when a drag is initiated. This is the right time to set our flag.
+     */
+    wasDraggedRef.current = true;
+  };
 
   const handleDragEnd = (event: MouseEvent | TouchEvent | PointerEvent, info: PanInfo) => {
     const { offset, velocity } = info;
+
+    /**
+     * Snap animation logic
+     */
     const swipeThreshold = -actionsWidth / 2;
     const velocityThreshold = -500;
 
@@ -24,13 +39,48 @@ export const SwipeTrigger: FC<PropsWithChildren<SwipeTriggerProps>> = ({ childre
     }
   };
 
+  const handleClickCapture = (event: React.MouseEvent | React.TouchEvent | React.PointerEvent) => {
+    /**
+     * Set a threshold to determine if the swipe is considered "open" or "closed"
+     */
+    const isFullyOpen = Math.abs(x.get() + actionsWidth) < 1;
+
+    /**
+     * If the trigger is fully open, the first click closes them.
+     */
+    if (isFullyOpen) {
+      event.stopPropagation();
+      event.preventDefault();
+      animate(x, 0, { type: 'spring', stiffness: 500, damping: 40 });
+      // Reset the flag for the next interaction
+      wasDraggedRef.current = false;
+      return;
+    }
+
+    // If it was a drag action, block the click
+    if (wasDraggedRef.current) {
+      event.stopPropagation();
+      event.preventDefault();
+    }
+  };
+
+  const handlePointerDown = () => {
+    // At the beginning of each new interaction (press), we reset the flag.
+    // If the user only clicks, the flag will remain false.
+    // If the user starts dragging, onDragStart will set it to true.
+    wasDraggedRef.current = false;
+  };
+
   return (
     <motion.div
       className={styles.Trigger}
       style={{ x }}
       drag="x"
       dragConstraints={{ left: -actionsWidth, right: 0 }}
+      onPointerDown={handlePointerDown}
+      onDragStart={handleDragStart}
       onDragEnd={handleDragEnd}
+      onClickCapture={handleClickCapture}
       whileDrag={{ cursor: 'grabbing' }}
     >
       {children}
