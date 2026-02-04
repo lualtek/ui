6da19ff3d7e4496d8a1fbef0ffc6d5de15d005ae
Alessio Occhipinti <LasaleFamine@users.noreply.github.com>
2026-02-04 16:04:47 +0100

    chore: added bundler config postcss with resolve (#689)

---
+++ .changeset/red-signs-float.md
@@ -0,0 +1,5 @@
+---
+"@lualtek/icons": patch
+---
+
+Update postcss configs
--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,6 +1,7 @@
 
-import { getConfigObject as getConfig } from './postcss/index.js';
+import { getConfigBundlerObject as getBundlerConfig, getConfigObject as getConfig } from './postcss/index.js';
 
 export const postcss = {
   getConfig,
+  getBundlerConfig,
 };
